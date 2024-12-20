672d71e3dd6b5b2336fc09b9768ff07adbb2f67f
Artem Bulkhak <artem.bulkhak@mail.ru>
2024-12-20 16:54:12 +0300

    Add practice4

---
+++ practice4/src/main/java/com/example/practice4/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.practice4;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    MyBroadcastReceiver myBroadcastReceiver;
+    Button buttonStartService;
+    Button buttonStopService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        IntentFilter filter = new IntentFilter("android.intent.action.AIRPLANE_MODE");
+        myBroadcastReceiver = new MyBroadcastReceiver();
+        registerReceiver(myBroadcastReceiver, filter);
+
+        buttonStartService = findViewById(R.id.buttonStart);
+        buttonStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServiceMediaPlayer.class));
+            }
+        });
+
+        buttonStopService = findViewById(R.id.buttonStop);
+        buttonStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServiceMediaPlayer.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(myBroadcastReceiver);
+        super.onDestroy();
+    }
+}
